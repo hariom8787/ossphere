56451320984166287b8c8d9040d797714e50ed71
akhilesh_verma <146834811+akhileshverma92@users.noreply.github.com>
2025-02-22 12:47:38 +0530

    Add files via upload

---
+++ Osshpere_/src/Pages/EventListing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EventListing() {
+  return (
+    <div>EventListing</div>
+  )
+}
+
+export default EventListing
---
+++ Osshpere_/src/Pages/EventRegister.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EventRegister() {
+  return (
+    <div>EventRegister</div>
+  )
+}
+
+export default EventRegister
